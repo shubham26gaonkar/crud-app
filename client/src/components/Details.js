d503a409d77cf65334fc13042f579e05fd85dcf5
Shubham Gaonkar <shubhamgaonkar26@gmail.com>
2023-05-08 18:25:11 +0530

    details updated

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -69,7 +69,7 @@ const Details = () => {
 
     return (
         <div className="container mt-3">
-            <h1 style={{ fontWeight: 400 }}>Welcome Harsh Pathak</h1>
+            <h1 style={{ fontWeight: 400 }}>Welcome</h1>
 
             <Card sx={{ maxWidth: 600 }}>
                 <CardContent>
